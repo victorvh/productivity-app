aa79c69cbad9c0050c6a2a24bd35d2f0d64d71ef
VictorVH <victor@skavi.dk>
2019-11-16 20:33:24 +0100

    added nav

---
+++ src/components/nav/button/index.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class Button extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+
+    }
+
+    
+    render() {
+        return(
+
+        );
+    }
+}
